73be4f6b4cfbad24f2e9fdbf77799c4eeda97157
Ichsyan Fahcrian <ichsyan.fachrian@gmail.com>
2022-05-26 21:41:19 +0700

    I made package from previous function with

--- main.py
+++ main.py
@@ -2,31 +2,9 @@
 INDONESIA EARTHQUAKE DETECTION APPLICATION
 I use function modularization
 """
-
-def data_extc():
-    show = dict()
-    show['date'] = "26 May 2022"
-    show['time'] = "03:24:45 WIB"
-    show['magnitude'] = 3.1
-    show['depth'] = "2 Km"
-    show['location'] = {'ltd': 5.89, 'lon': 95.19}
-    show['position'] = "The epicenter of the earthquake was in the sea 14 km southwest of Sabang City"
-    show['scala'] = "MMI Scale at I-II Sabang City"
-
-    return show
-
-
-def data_vis(result):
-    print('ATTENTION!!! INDONESIAN EARTHQUAKE UPDATE :')
-    print(f"Date : {result['date']}")
-    print(f"Time : {result['time']}")
-    print(f"Magnitude : {result['magnitude']}")
-    print(f"Depth : {result['depth']}")
-    print(f"Location : LTD : {result['location']['ltd']} LON : {result['location']['lon']}")
-    print(f"Position : {result['position']}")
-
+import indomagnitude
 
 if __name__ == '__main__':
     print('Main Application')
-    result = data_extc()
-    data_vis(result)
+    result = indomagnitude.data_extc()
+    indomagnitude.data_vis(result)
