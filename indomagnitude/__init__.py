c5a422ba208fda45368fe7ad5a27f443e571511c
Ichsyan Fahcrian <ichsyan.fachrian@gmail.com>
2022-05-29 22:44:00 +0700

    I use some package from pypi.org, that is BS4 & Request to get some data from website

--- indomagnitude/__init__.py
+++ indomagnitude/__init__.py
@@ -1,14 +1,31 @@
-def data_extc():
-    show = dict()
-    show['date'] = "26 May 2022"
-    show['time'] = "03:24:45 WIB"
-    show['magnitude'] = 3.1
-    show['depth'] = "2 Km"
-    show['location'] = {'ltd': 5.89, 'lon': 95.19}
-    show['position'] = "The epicenter of the earthquake was in the sea 14 km southwest of Sabang City"
-    show['scala'] = "MMI Scale at I-II Sabang City"
+import requests
+from bs4 import BeautifulSoup
 
-    return show
+
+def data_extc():
+
+    try:
+        content = requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
+
+
+    if content.status_code == 200:
+        soup = BeautifulSoup(content.text, 'html.parser')
+        find = soup.find('span', {'class': 'waktu'})
+        dateTime = find.text.split(', ')
+
+        show = dict()
+        show['date'] = dateTime[0]
+        show['time'] = dateTime[1]
+        show['magnitude'] = 3.1
+        show['depth'] = "2 Km"
+        show['location'] = {'ltd': 5.89, 'lon': 95.19}
+        show['position'] = "The epicenter of the earthquake was in the sea 14 km southwest of Sabang City"
+        show['scala'] = "MMI Scale at I-II Sabang City"
+        return show
+    else:
+        return None
 
 
 def data_vis(result):
