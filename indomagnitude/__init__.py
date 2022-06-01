0098688a9c1a45c6190122f0d6f9847814a74e93
Ichsyan Fahcrian <ichsyan.fachrian@gmail.com>
2022-06-01 21:06:21 +0700

    Finishing my package to scraping earthquake information in my country

--- indomagnitude/__init__.py
+++ indomagnitude/__init__.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def data_extc():
 
     try:
@@ -12,17 +11,51 @@ def data_extc():
 
     if content.status_code == 200:
         soup = BeautifulSoup(content.text, 'html.parser')
-        find = soup.find('span', {'class': 'waktu'})
-        dateTime = find.text.split(', ')
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+
+        #VARIABLE FOR FILLING CONTENT
+        i = 0
+        dateTime = None
+        date = None
+        time = None
+        magnitude = None
+        depth = None
+        coord = None
+        ltd = None
+        lon = None
+        position = None
+        scala = None
+
+        for fill in result:
+            i = i + 1
+
+            if i == 1:
+                dateTime = fill.text.split(', ')
+                date = dateTime[0]
+                time = dateTime[1]
+            elif i == 2:
+                magnitude = fill.text
+            elif i == 3:
+                depth = fill.text
+            elif i == 4:
+                coord = fill.text.split(' - ')
+                ltd = coord[0]
+                lon = coord[1]
+            elif i == 5:
+                position = fill.text
+            elif i == 6:
+                scala = fill.text
+
 
         show = dict()
-        show['date'] = dateTime[0]
-        show['time'] = dateTime[1]
-        show['magnitude'] = 3.1
-        show['depth'] = "2 Km"
-        show['location'] = {'ltd': 5.89, 'lon': 95.19}
-        show['position'] = "The epicenter of the earthquake was in the sea 14 km southwest of Sabang City"
-        show['scala'] = "MMI Scale at I-II Sabang City"
+        show['date'] = date
+        show['time'] = time
+        show['magnitude'] = magnitude
+        show['depth'] = depth
+        show['coord'] = {'ltd': ltd, 'lon': lon}
+        show['position'] = position
+        show['scala'] = scala
         return show
     else:
         return None
@@ -34,5 +67,6 @@ def data_vis(result):
     print(f"Time : {result['time']}")
     print(f"Magnitude : {result['magnitude']}")
     print(f"Depth : {result['depth']}")
-    print(f"Location : LTD : {result['location']['ltd']} LON : {result['location']['lon']}")
+    print(f"Coordination : LTD : {result['coord']['ltd']} LON : {result['coord']['lon']}")
     print(f"Position : {result['position']}")
+    print(f"Scala : {result['scala']}")
